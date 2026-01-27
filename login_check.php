8d88fcd7f9a96a9c99111dd86ca782c6fb0732f4
Alex Leiva <alex.leiva230@gmail.com>
2026-01-27 14:03:38 -0500

    Add files via upload

--- login_check.php
+++ login_check.php
@@ -35,12 +35,20 @@ if ($user != $_POST["login_user"]) {
 
 $password = addslashes($_POST["login_password"]);
 
-if ($password != $_POST["login_password"]);
-	die("Error 7: ");
+if ($password != $_POST["login_password"]){
+	die("Error 7: Password mal formulado ");
+}
 
-
+if (strlen($password) < 4){
+	die("Error 8: Password demasiado corto");
+}
 
 $password = md5($password);
+
+$conn = mysqli_connect('localhost', 'admin_db', 'enti', 'pizzeria_musso');
+if (!$conn){
+	die("Error 9: No se pudo conectar con la base de datos");
+}
 
 $query = <<<EOD
 SELECT
@@ -48,8 +56,21 @@ SELECT
 FROM
 	users
 WHERE
-	name='($user)'
+	username='($user)'
 	AND password='{$password}';
 EOD;
+
+echo $query;
+
+$result = mysqli_query($conn, $query);
+if (!$result){
+	die("Error 10: Petición incorrecta");
+}
+
+if (mysqli_num_rows($result) != 1){
+	die ("Error 11: Nombre de usuario o password incorrecto");
+}
+
+echo "logueado";
 
 ?>
